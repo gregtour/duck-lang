deb59226331c98c7fa18a19505df7c61f6e62309
rpcope1 <rpcope1@gmail.com>
2014-11-20 00:38:15 -0700

    Swapped out built-in random number generator for XORSHIFT128+ PRNG, simplified library functions via optional arguments. Still needs some work.

--- duck/lib/drand.c
+++ duck/lib/drand.c
@@ -1,58 +1,106 @@
 /*
+  Duck Language Random Number Library
   Author: Robert Cope
 
   TODO:
-    * Expand beyond basic C random number algorithm?
+    * More functions? 
+    * Clean up code, make sure implementation of XORSHIFT works well.
 
 */
 #include <stdlib.h>
-#include "dmath.h"
+#include <stdint.h>
+#include <time.h>
+#include <limits.h>
+#include <math.h>
+
+#include "drand.h"
 #include "interpreter.h"
 
-/*Rand.seed(seed_num)*/
+
+/*  XORShift128+ Written in 2014 by Sebastiano Vigna (vigna@acm.org)
+
+To the extent possible under law, the author has dedicated all copyright
+and related and neighboring rights to this software to the public domain
+worldwide. This software is distributed without any warranty.
+
+See <http://creativecommons.org/publicdomain/zero/1.0/>. */
+
+uint64_t xor_s[ 2 ] = {0x021f9c6a70ca0c24ULL, 0xbae766b26645aa22ULL};
+
+uint64_t xorshift_next(void) { 
+	uint64_t s1 = xor_s[ 0 ];
+	const uint64_t s0 = xor_s[ 1 ];
+	xor_s[ 0 ] = s0;
+	s1 ^= s1 << 23; // a
+	return ( xor_s[ 1 ] = ( s1 ^ s0 ^ ( s1 >> 17 ) ^ ( s0 >> 26 ) ) ) + s0; // b, c
+}
+
+const uint64_t XORSHIFT_MAX = 0xffffffffffffffffULL;
+
+
+
+/*Rand.seed((OPTIONAL) seed_num)*/
+/*Returns the seed used.*/
 int SeedRand(int arg_count){
   VALUE argument = GetRecord("seed_number", gCurrentContext);
 
-  unsigned int seed = TypeInt(argument);
+  //TODO: Seeding the XORSHIFT128+ with the built in generator is somewhat questionable.
+  int seed = 0;
+  if(argument.type != VAL_NIL)
+    seed = TypeInt(argument);
+  else
+    seed = time(NULL);
+    
   srand(seed);
+  //Seed the XORSHIFT128+ PRNG with whatever junky random number generator was built in.
+  xor_s[0] = ((long long)rand() << 32) | rand();
+  xor_s[1] = ((long long)rand() << 32) | rand();
+
+  gLastExpression.type = VAL_PRIMITIVE;
+  gLastExpression.data.primitive = seed;
 
   return 0;
 }
 
-int GetRand(int arg_count){
-    int random_num = rand();
 
-    gLastExpression.type = VAL_PRIMITIVE;
-    gLastExpression.data.primitive = random_num;
 
-    return 0;
-}
-
+/*Rand.rand((OPTIONAL) first_bound, (OPTIONAL) second_bound)*/
+/*Returns a random primitive*/
 int GetBoundedRand(int arg_count){
     VALUE fb_arg = GetRecord("first_bound", gCurrentContext);
     VALUE sb_arg = GetRecord("second_bound", gCurrentContext);
 
     int num = 0;
-    int lower_bound = TypeInt(fb_arg);
-    int upper_bound = TypeInt(sb_arg);
-
-    //Swap if user has misordered bounds.
+    long lower_bound = 0;
+    long upper_bound = 0;
+    if(fb_arg.type != VAL_NIL) 
+        lower_bound = TypeInt(fb_arg);
+    else
+        lower_bound = INT_MIN;
+    if(sb_arg.type != VAL_NIL)
+        upper_bound = TypeInt(sb_arg);
+    else        
+        upper_bound = INT_MAX;
+    
+    //Swap if user has misordered bounds..
     if(upper_bound < lower_bound){
       int tmp = lower_bound;
       lower_bound = upper_bound;
       upper_bound = tmp;
     }
-
-    num = (rand() % (upper_bound - lower_bound)) + lower_bound;
-
+ 
+    //TODO: Fix this hack.
+    num = (int)((xorshift_next() & UINT_MAX) % (upper_bound - lower_bound + 1)) + lower_bound;
     gLastExpression.type = VAL_PRIMITIVE;
     gLastExpression.data.primitive = num;
 
     return 0;
-
-
 }
 
+
+
+/*Rand.randfloat(first_bound, second_bound)*/
+/*Returns a random float.*/
 int GetBoundedRandFloat(int arg_count){
     VALUE fb_arg = GetRecord("first_bound", gCurrentContext);
     VALUE sb_arg = GetRecord("second_bound", gCurrentContext);
@@ -69,14 +117,12 @@ int GetBoundedRandFloat(int arg_count){
     }
 
 
-    num =  (rand() * (upper_bound - lower_bound))/(RAND_MAX + 1.0) + lower_bound;
+    num =   (xorshift_next() * (upper_bound - lower_bound))/(XORSHIFT_MAX) + lower_bound;
 
     gLastExpression.type = VAL_FLOATING_POINT;
     gLastExpression.data.floatp = num;
 
     return 0;
-
-
 }
 
 void BindRandLibrary()
@@ -88,13 +134,11 @@ void BindRandLibrary()
   AddParameter(rand_seedrand, "seed_number");
   LinkFunction(rand_lib, "seed", rand_seedrand);
 
-  VALUE rand_getrand = CreateFunction(GetRand);
+  VALUE rand_getrand = CreateFunction(GetBoundedRand);
+  AddParameter(rand_getrand, "first_bound");
+  AddParameter(rand_getrand, "second_bound");
   LinkFunction(rand_lib, "rand", rand_getrand);
 
-  VALUE rand_getboundedrand = CreateFunction(GetBoundedRand);
-  AddParameter(rand_getboundedrand, "first_bound");
-  AddParameter(rand_getboundedrand, "second_bound");
-  LinkFunction(rand_lib, "boundedrand", rand_getboundedrand);
   VALUE rand_getrandfloat = CreateFunction(GetBoundedRandFloat);
   AddParameter(rand_getrandfloat, "first_bound");
   AddParameter(rand_getrandfloat, "second_bound");
