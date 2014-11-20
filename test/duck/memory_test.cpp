08c0f840b008da1144fc15e37094b58702e06708
Hiroyuki Sano <sh19910711@gmail.com>
2014-11-20 14:24:34 +0900

    Add memory_test.cpp

---
+++ test/duck/memory_test.cpp
@@ -0,0 +1,53 @@
+#include <gtest/gtest.h>
+
+extern "C"
+{
+#include "memory.h"
+}
+
+TEST(Memory, get_undefined_record)
+{
+    CreateEnvironment();
+
+    // create context
+    CONTEXT* context = (CONTEXT*)ALLOC(sizeof(CONTEXT));
+    context->parent = NULL;
+    context->list = NULL;
+    context->ref_count = -1;
+
+    // store string record
+    VALUE v_str;
+    v_str.type = VAL_STRING;
+    v_str.data.string = "world";
+    StoreRecord("hello", v_str, context);
+
+    // get undefiend record
+    VALUE v_ret = GetRecord("hello2", context);
+    ASSERT_EQ(v_ret.type, VAL_NIL);
+
+    FreeEnvironment();
+}
+
+TEST(Memory, store_and_get_string_record)
+{
+    CreateEnvironment();
+
+    // create context
+    CONTEXT* context = (CONTEXT*)ALLOC(sizeof(CONTEXT));
+    context->parent = NULL;
+    context->list = NULL;
+    context->ref_count = -1;
+
+    // store string record
+    VALUE v_str;
+    v_str.type = VAL_STRING;
+    v_str.data.string = "world";
+    StoreRecord("hello", v_str, context);
+
+    VALUE v_ret = GetRecord("hello", context);
+    ASSERT_EQ(v_ret.type, VAL_STRING);
+    ASSERT_EQ(v_ret.data.string, "world");
+
+    FreeEnvironment();
+}
+
