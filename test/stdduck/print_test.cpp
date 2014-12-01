9e230ec537a6e28dd2acbcb78a35e1fe95341c67
Hiroyuki Sano <sh19910711@gmail.com>
2014-12-01 16:03:48 +0900

    Add tests for stdduck#PrintValue()
    * output string
    * output function

---
+++ test/stdduck/print_test.cpp
@@ -0,0 +1,44 @@
+#include <gtest/gtest.h>
+
+extern "C"
+{
+#include "lib/stdduck.h"
+}
+
+TEST(StdDuck, print_value_string)
+{
+    CreateEnvironment();
+    testing::internal::CaptureStdout();
+
+    VALUE val;
+    val.type = VAL_STRING;
+    val.data.string = "1";
+
+    PrintValue(val);
+    ASSERT_STREQ("1", testing::internal::GetCapturedStdout().c_str());
+
+    FreeEnvironment();
+}
+
+namespace TestStdDuck {
+  int DummyFunc(int num_args) {
+      int error = 0;
+      VALUE argument = GetRecord("arg0", gCurrentContext);
+      return error;
+  }
+}
+
+TEST(StdDuck, print_value_function)
+{
+    CreateEnvironment();
+    testing::internal::CaptureStdout();
+
+    VALUE func = CreateFunction(TestStdDuck::DummyFunc);
+    AddParameter(func, "arg0");
+
+    PrintValue(func);
+    ASSERT_STREQ("function(arg0)", testing::internal::GetCapturedStdout().c_str());
+
+    FreeEnvironment();
+}
+
